de90c1c4c5280bdff9ea5b5974ca4d8e75bc2d85
nagendra <nagendra@nagendra-HP-Notebook>
2017-10-15 03:49:18 +0530

    Commited

---
+++ src/practiceCoreJava1/Sample.java
@@ -0,0 +1,15 @@
+package practiceCoreJava1;
+
+public class Sample {
+	public static void main(String[] args) {
+		int n,a,g;
+		n=1;
+		a=2;
+		g=3;
+		
+		System.out.print(n+""+a+""+g);
+		System.out.println(" Is a Great Guy");
+		
+	}
+
+}
